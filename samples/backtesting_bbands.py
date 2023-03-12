24d59f47c83feeaaad0f6d1ddf0086e81879cc04
Gabriel Becedillas <gabriel.becedillas@gmail.com>
2023-03-11 23:50:44 -0300

    Updated samples to use minute bars and to use a fixed amount when opening a position.

--- samples/backtesting_bbands.py
+++ samples/backtesting_bbands.py
@@ -19,8 +19,8 @@
 """
 
 # Bars can be downloaded using this command:
-# python -m basana.external.binance.tools.download_bars -c BTC/USDT -p 1d -s 2017-01-01 -e 2021-12-31 > \
-# binance_btcusdt_day.csv
+# python -m basana.external.binance.tools.download_bars -c BTC/USDT -p 1m -s 2021-01-01 -e 2021-01-31 > \
+# binance_btcusdt_min.csv
 
 from decimal import Decimal
 import asyncio
@@ -34,25 +34,25 @@ import bbands
 
 # The strategy is responsible for placing orders in response to trading signals.
 class Strategy:
-    def __init__(self, exchange: backtesting_exchange.Exchange, position_pct: Decimal):
-        assert position_pct > 0 and position_pct <= 1
+    def __init__(self, exchange: backtesting_exchange.Exchange, position_amount: Decimal):
+        assert position_amount > 0
         self._exchange = exchange
-        self._position_pct = position_pct
+        self._position_amount = position_amount
 
     async def on_trading_signal(self, trading_signal: bs.TradingSignal):
         logging.info("Trading signal: operation=%s pair=%s", trading_signal.operation, trading_signal.pair)
         try:
             # Calculate the order size.
+            balances = await self._exchange.get_balances()
             if trading_signal.operation == bs.OrderOperation.BUY:
-                balance, (_, ask) = await asyncio.gather(
-                    self._exchange.get_balance(trading_signal.pair.quote_symbol),
-                    self._exchange.get_bid_ask(trading_signal.pair)
-                )
-                order_size = balance.available * self._position_pct / ask
+                _, ask = await self._exchange.get_bid_ask(trading_signal.pair)
+                balance = balances[trading_signal.pair.quote_symbol]
+                order_size = min(self._position_amount, balance.available) / ask
             else:
-                balance = await self._exchange.get_balance(trading_signal.pair.base_symbol)
+                balance = balances[trading_signal.pair.base_symbol]
                 order_size = balance.available
-            order_size = bs.truncate_decimal(order_size, 8)
+            pair_info = await self._exchange.get_pair_info(trading_signal.pair)
+            order_size = bs.truncate_decimal(order_size, pair_info.base_precision)
             if not order_size:
                 return
 
@@ -69,19 +69,22 @@ async def main():
 
     event_dispatcher = bs.backtesting_dispatcher()
     pair = bs.Pair("BTC", "USDT")
-    exchange = backtesting_exchange.Exchange(event_dispatcher, initial_balances={"USDT": Decimal(10000)})
+    exchange = backtesting_exchange.Exchange(
+        event_dispatcher,
+        initial_balances={"BTC": Decimal(0), "USDT": Decimal(10000)}
+    )
     exchange.set_pair_info(pair, bs.PairInfo(8, 2))
 
     # Connect the signal source with the bar events from the exchange.
-    signal_source = bbands.SignalSource(event_dispatcher, 23, 3.1)
+    signal_source = bbands.SignalSource(event_dispatcher, 20, 1.5)
     exchange.subscribe_to_bar_events(pair, signal_source.on_bar_event)
 
     # Connect the strategy to the trading signal source.
-    strategy = Strategy(exchange, Decimal("0.95"))
+    strategy = Strategy(exchange, Decimal(1000))
     signal_source.subscribe_to_trading_signals(strategy.on_trading_signal)
 
     # Load bars from CSV files.
-    exchange.add_bar_source(csv.BarSource(pair, "binance_btcusdt_day.csv", "1d"))
+    exchange.add_bar_source(csv.BarSource(pair, "binance_btcusdt_min.csv", "1m"))
 
     # Run the backtest.
     await event_dispatcher.run()
--- samples/backtesting_rsi.py
+++ samples/backtesting_rsi.py
@@ -19,8 +19,8 @@
 """
 
 # Bars can be downloaded using this command:
-# python -m basana.external.bitstamp.tools.download_bars -c BTC/USD -p day -s 2015-01-01 -e 2021-12-31 > \
-# bitstamp_btcusd_day.csv
+# python -m basana.external.bitstamp.tools.download_bars -c BTC/USD -p 1m -s 2021-01-01 -e 2021-01-31 > \
+# bitstamp_btcusd_min.csv
 
 from decimal import Decimal
 import asyncio
@@ -34,10 +34,10 @@ import rsi
 
 # The strategy is responsible for placing orders in response to trading signals.
 class Strategy:
-    def __init__(self, exchange: backtesting_exchange.Exchange, position_pct: Decimal):
-        assert position_pct > 0 and position_pct <= 1
+    def __init__(self, exchange: backtesting_exchange.Exchange, position_amount: Decimal):
+        assert position_amount > 0
         self._exchange = exchange
-        self._position_pct = position_pct
+        self._position_amount = position_amount
 
     async def calculate_price(self, trading_signal: bs.TradingSignal):
         bid, ask = await self._exchange.get_bid_ask(trading_signal.pair)
@@ -63,23 +63,24 @@ class Strategy:
             )
 
             # Calculate the order price and size.
-            balance_symbol = trading_signal.pair.quote_symbol if trading_signal.operation == bs.OrderOperation.BUY \
-                else trading_signal.pair.base_symbol
-            balance, price, pair_info = await asyncio.gather(
-                self._exchange.get_balance(balance_symbol),
+            balances, price, pair_info = await asyncio.gather(
+                self._exchange.get_balances(),
                 self.calculate_price(trading_signal),
                 self._exchange.get_pair_info(trading_signal.pair)
             )
             if trading_signal.operation == bs.OrderOperation.BUY:
-                order_size = balance.available * self._position_pct / price
+                balance = balances[trading_signal.pair.quote_symbol]
+                order_size = min(self._position_amount, balance.available) / price
             else:
+                balance = balances[trading_signal.pair.base_symbol]
                 order_size = balance.available
             order_size = bs.truncate_decimal(order_size, pair_info.base_precision)
             if not order_size:
                 return
 
             logging.info(
-                "Creating %s limit order for %s. Price: %s", trading_signal.operation, trading_signal.pair, price
+                "Creating %s limit order for %s: amount: %s price: %s", trading_signal.operation, trading_signal.pair,
+                order_size, price
             )
             await self._exchange.create_limit_order(
                 trading_signal.operation, trading_signal.pair, order_size, price
@@ -92,20 +93,23 @@ async def main():
     logging.basicConfig(level=logging.INFO, format="[%(asctime)s %(levelname)s] %(message)s")
 
     event_dispatcher = bs.backtesting_dispatcher()
-    pair = bs.Pair("BTC", "USDT")
-    exchange = backtesting_exchange.Exchange(event_dispatcher, initial_balances={"USDT": Decimal(10000)})
+    pair = bs.Pair("BTC", "USD")
+    exchange = backtesting_exchange.Exchange(
+        event_dispatcher,
+        initial_balances={"BTC": Decimal(0), "USD": Decimal(10000)}
+    )
     exchange.set_pair_info(pair, bs.PairInfo(8, 2))
 
     # Connect the signal source with the bar events from the exchange.
-    signal_source = rsi.SignalSource(event_dispatcher, 25, 39, 79)
+    signal_source = rsi.SignalSource(event_dispatcher, 20, 30, 70)
     exchange.subscribe_to_bar_events(pair, signal_source.on_bar_event)
 
     # Connect the strategy to the trading signal source.
-    strategy = Strategy(exchange, Decimal("0.95"))
+    strategy = Strategy(exchange, Decimal(1000))
     signal_source.subscribe_to_trading_signals(strategy.on_trading_signal)
 
     # Load bars from CSV files.
-    exchange.add_bar_source(csv.BarSource(pair, "bitstamp_btcusd_day.csv", "1d"))
+    exchange.add_bar_source(csv.BarSource(pair, "bitstamp_btcusd_min.csv", "1d"))
 
     # Run the backtest.
     await event_dispatcher.run()
