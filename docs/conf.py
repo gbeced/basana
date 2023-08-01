c95e9e3923f75411319a92ce370f537979d90443
Gabriel Becedillas <gabriel.becedillas@gmail.com>
2023-08-01 13:22:48 -0300

    Updated doc scheme because some pages were not being properly rendered.

--- docs/conf.py
+++ docs/conf.py
@@ -29,7 +29,7 @@ autodoc_typehints_format = 'short'
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinxdoc'
 html_static_path = ['_static']
 html_theme_options = {
 }
