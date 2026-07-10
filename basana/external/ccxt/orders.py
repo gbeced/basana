2425964d1c840bdc753a7b359e013d46ac8958e9
Gabriel Becedillas <gabriel.becedillas@gmail.com>
2026-07-10 19:18:11 -0300

    Normalized clientOrderId to align with type hint.
    https://github.com/gbeced/basana/pull/59#discussion_r3562230253

--- basana/external/ccxt/orders.py
+++ basana/external/ccxt/orders.py
@@ -53,7 +53,7 @@ class Order:
     @property
     def client_order_id(self) -> Optional[str]:
         """The client order id."""
-        return self.raw.get("clientOrderId")
+        return None if (client_order_id := self.raw.get("clientOrderId")) is None else str(client_order_id)
 
     @property
     def operation(self) -> OrderOperation:
